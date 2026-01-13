27b919be66d00d14e249d52bcf3dd748168fd775
MatthewKim323 <matthewykim23@gmail.com>
2026-01-13 02:41:42 -0800

    Remove backend, keep only landing page

--- backend/database/__init__.py
+++
@@ -1,4 +0,0 @@
-from .database import SessionLocal, engine, Base
-from .models import User, Workout, Analysis, RepData
-
-__all__ = ["SessionLocal", "engine", "Base", "User", "Workout", "Analysis", "RepData"]
